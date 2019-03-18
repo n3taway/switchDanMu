afaf502f62c1239a47108cd0747ca65c554ef206
n3taway <n3taway@gmail.com>
2019-03-19 01:19:21 +0800

    支持A站，B站，斗鱼

--- switchDanMu.user.js
+++ switchDanMu.user.js
@@ -5,67 +5,97 @@
 // @description  不支持flash播放器。shift+D 打开或关闭弹幕
 // @author       n3taway
 // @match        http://www.acfun.cn/*
-// @match        https://www.bilibili.com//*
+// @match        https://www.bilibili.com/*
 // @match        https://www.huya.com/*
+// @match        https://www.douyu.com/*
 // @license      MIT
 // @grant        none
 // ==/UserScript==
 
 (function () {
     'use strict';
-    var _switchDanMuMsg = document.createElement('div');
-    _switchDanMuMsg.style.height = '40px';
-    _switchDanMuMsg.style.lineHeight = '40px';
-    _switchDanMuMsg.style.width = '100px';
-    _switchDanMuMsg.style.background = '#fff';
-    _switchDanMuMsg.style.color = 'rgba(0, 0, 0, 0.65)';
-    _switchDanMuMsg.style.boxShadow = '0 4px 12px rgba(0, 0, 0, 0.15)';
-    _switchDanMuMsg.style.position = 'absolute';
-    _switchDanMuMsg.style.left = 'calc(50% - 50px)';
-    _switchDanMuMsg.style.top = '10%';
-    _switchDanMuMsg.style.borderRadius = '5% ';
-    _switchDanMuMsg.style.textAlign = 'center';
-    _switchDanMuMsg.style.display = 'none';
-    _switchDanMuMsg.style.zIndex = '999999';
+    const toolTip = document.createElement('div');
+    toolTip.setAttribute('style', `
+        width: 100px;
+        height: 40px;
+        line-height: 40px;
+        background: #fff;
+        color: rgba(0, 0, 0, 0.65);
+        box-shadow: 0 4px 12px rgba(0, 0, 0, 0.15);
+        position: absolute;
+        left: calc(50% - 50px);
+        top: 10%;
+        border-radius: 5%;
+        text-align: center;
+        display: none;
+        pointer-events: none;
+        z-index: 998;
+    `);
 
-    var tvConfig = new Map()
-        .set('虎牙', {
-            url: 'huya.com',
-            btnNode: '#player-danmu-btn', //开关按钮
-            danmuwrapNode: '#danmuwrap', //弹幕包裹层
-            optionwrapNode: '.player-danmu-pane', //弹幕操作提示层
-        })
-        .set('B站', {
-            url: 'bilibili.com',
-            btnNode: '.bilibili-player-video-btn.bilibili-player-video-btn-danmaku',
-            danmuwrapNode: '.bilibili-player-video-danmaku',
-            optionwrapNode: '.bilibili-player-danmaku-setting-lite-panel',
-        })
+    const toolTipWrap = document.createElement('div');
+    toolTipWrap.setAttribute('id','_toolTipWrap_');
+    toolTipWrap.setAttribute('style', `
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        pointer-events: none;
+        z-index: 999;
+        display: block;
+    `);
+    toolTipWrap.appendChild(toolTip);
+
+    const tvConfig = new Map()
         .set('A站', {
+            doubleBtn: false,
             url: 'acfun.cn',
             btnNode: '.danmu-enabled',
-            danmuwrapNode: '.danmu-container',
-            optionwrapNode: 'noDiv',
+            optionWrapNode: 'noDiv',
+        })
+        .set('B站', {
+            doubleBtn: false,
+            url: 'bilibili.com',
+            btnNode: '.bilibili-player-video-btn.bilibili-player-video-btn-danmaku',
+            optionWrapNode: '.bilibili-player-danmaku-setting-lite-panel',
+        })
+        .set('虎牙', {
+            doubleBtn: false,
+            url: 'huya.com',
+            btnNode: '#player-danmu-btn', //开关按钮
+            optionWrapNode: '.player-danmu-pane', //弹幕操作提示层
+        })
+        .set('斗鱼', {
+            doubleBtn: true,
+            url: 'douyu.com',
+            btnNode: '.showdanmu-42b0ac', //开启按钮
+            btnOffNode: '.hidedanmu-5d54e2', //关闭按钮
+            optionWrapNode: 'noDiv',
         });
-    var [tvInfo] = [...tvConfig.values()].filter(item => location.href.includes(item.url));
-    var { btnNode, danmuwrapNode, optionwrapNode } = tvInfo;
 
-    var switchDanMu = {
+    const [tvInfo] = [...tvConfig.values()].filter(item => location.href.includes(item.url));
+    const { doubleBtn, btnNode, btnOffNode, optionWrapNode } = tvInfo;
+
+    const q = function (selector) {
+        return document.querySelector(selector);
+    };
+
+    const switchDanMu = {
         btn: undefined,
         showDanMu: true,
-        eventClick: new MouseEvent('click', {
+        click: new MouseEvent('click', {
             'view': window,
             'bubbles': true,
             'cancelable': true
         }),
-        getBtn: () => {
-            var timer = setInterval(() => {
-                var btn = document.querySelector(btnNode); //按钮
+
+        getBtn() {
+            const timer = setInterval(() => {
+                const btn = q(btnNode); //按钮
                 if (btn) {
                     switchDanMu.btn = btn;
                     switchDanMu.toggleDanMu();
-                    // document.querySelector(danmuwrapNode).appendChild(_switchDanMuMsg); //弹幕包裹层添加提示框
-                    document.querySelector(danmuwrapNode).insertBefore(_switchDanMuMsg,document.querySelector(danmuwrapNode).children[0])
+                    q('video').parentNode.appendChild(toolTipWrap); //提示信息与video同层级
                     console.warn('弹幕开关running...');
                     clearInterval(timer)
                 } else {
@@ -73,35 +103,63 @@
                 }
             }, 1000);
         },
-        toggleDanMu: () => {
+
+        showToolTip() {
+            toolTip.style.display = 'block';
+            toolTip.innerText = `弹幕已${switchDanMu.showDanMu ? '开' : '关'}`;
+            let top = 10;
+            setTimeout(() => {
+                const timer = setInterval(() => {
+                    if (top <= -10) {
+                        toolTipWrap.style.display = 'none';
+                        toolTipWrap.style.top = '10%';
+                        clearInterval(timer);
+                    } else {
+                        top -= 1;
+                        toolTipWrap.style.top = `${top}%`;
+                    }
+                }, 15);
+            }, 1 * 1000);
+        },
+
+        hideTvToolTip() {
+            if (q(optionWrapNode)) {
+                q(optionWrapNode).style.display = 'none';
+            };
+        },
+
+        doubleSwitch() {
+            if (switchDanMu.showDanMu) {
+                switchDanMu.btn.dispatchEvent(switchDanMu.click);
+                switchDanMu.showDanMu = !switchDanMu.showDanMu;
+            } else {
+                q(btnOffNode).dispatchEvent(switchDanMu.click);
+                switchDanMu.showDanMu = !switchDanMu.showDanMu;
+            };
+            switchDanMu.showToolTip();
+            switchDanMu.hideTvToolTip();
+        },
+
+        toggleDanMu() {
             document.addEventListener('keyup', e => {
                 if (e.shiftKey && e.keyCode === 68) {
-                    switchDanMu.btn.dispatchEvent(switchDanMu.eventClick);
-                    switchDanMu.showDanMu = !switchDanMu.showDanMu;
-                    if (document.querySelector(optionwrapNode)) {
-                        document.querySelector(optionwrapNode).style.display = 'none'
+                    q('#_toolTipWrap_').style.display = 'block';
+                    if (doubleBtn) {
+                        switchDanMu.doubleSwitch();
+                        return false;
                     };
-                    _switchDanMuMsg.style.display = 'block';
-                    _switchDanMuMsg.innerText = `弹幕已${switchDanMu.showDanMu ? '开' : '关'}`;
-                    var top = 10;
-                    setTimeout(() => {
-                        var timer = setInterval(() => {
-                            if (top <= -10) {
-                                _switchDanMuMsg.style.display = 'none';
-                                _switchDanMuMsg.style.top = '10%';
-                                clearInterval(timer);
-                            } else {
-                                top -= 1;
-                                _switchDanMuMsg.style.top = `${top}%`;
-                            }
-                        }, 15);
-                    }, 1 * 1000);
+                    switchDanMu.showDanMu = !switchDanMu.showDanMu;
+                    switchDanMu.btn.dispatchEvent(switchDanMu.click);
+                    switchDanMu.showToolTip();
+                    switchDanMu.hideTvToolTip();
                 }
             });
         },
-        run: () => {
+
+        run() {
             switchDanMu.getBtn();
         },
     }
-    switchDanMu.run();;
+
+    switchDanMu.run();
 })();
